2f713ac5848d4bc4809b6dbb2584d768dca9cde2
Aditya <aditya.ace@hotmail.com>
2020-03-29 01:52:03 +0530

    fixed z-index issue, still not sure how layering works

--- src/components/GlobalReport.js
+++ src/components/GlobalReport.js
@@ -10,29 +10,35 @@ const GlobalReport = () => {
   const globalReport = coronaCasesContext.globalReport;
 
   return (
-    <div className="global-content">
-      {!globalReport.cases ? (
-        <img src={Loader} alt="Loader" />
-      ) : (
-        <>
-          <h1>Global Report</h1>
-          <span style={{ color: "#FFC300" }}>
-            Last Updated on : <Moment date={globalReport.updated} />
-          </span>
-          <h5>Cases:</h5>
-          <p style={{ color: "#C70039" }}>
-            {Number(globalReport.cases).toLocaleString()}
-          </p>
-          <h5>Deaths:</h5>
-          <p style={{ color: "#C70039" }}>
-            {Number(globalReport.deaths).toLocaleString()}
-          </p>
-          <h5>Recovered:</h5>
-          <p style={{ color: "#C70039" }}>
-            {Number(globalReport.recovered).toLocaleString()}
-          </p>
-        </>
-      )}
+    <div className="container">
+      <div className="row">
+        <div className="col s12">
+          <div className="global-content">
+            {!globalReport.cases ? (
+              <img src={Loader} alt="Loader" style={{ marginTop: "10em" }} />
+            ) : (
+              <>
+                <h1>Global Report</h1>
+                <span className="yellow-text accent-2">
+                  Last Updated on : <Moment date={globalReport.updated} />
+                </span>
+                <h5>Cases:</h5>
+                <p className="red-text accent-4">
+                  {Number(globalReport.cases).toLocaleString()}
+                </p>
+                <h5>Deaths:</h5>
+                <p className="red-text accent-4">
+                  {Number(globalReport.deaths).toLocaleString()}
+                </p>
+                <h5>Recovered:</h5>
+                <p className="red-text accent-4">
+                  {Number(globalReport.recovered).toLocaleString()}
+                </p>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/VideoContent.js
+++ src/components/VideoContent.js
@@ -8,9 +8,9 @@ const VideoContent = () => {
         <div className="col s12">
           <Overlay />
           <div className="video-content">
-            <h1>COVID - 19</h1>
-            <h5>About the Virus</h5>
-            <p>
+            <h1 className="pink-text accent-3">COVID - 19</h1>
+            <h5 className="pink-text lighten-3">About the Virus</h5>
+            <p className="flow-text">
               Coronavirus disease (COVID-19) is an infectious disease caused by
               a new virus. The disease causes respiratory illness (like the flu)
               with symptoms such as a cough, fever, and in more severe cases,
@@ -18,8 +18,8 @@ const VideoContent = () => {
               hands frequently, avoiding touching your face, and avoiding close
               contact (1 meter or 3 feet) with people who are unwell.
             </p>
-            <h5>How it Spreads</h5>
-            <p>
+            <h5 className="pink-text lighten-3">How it Spreads</h5>
+            <p className="flow-text">
               Coronavirus disease spreads primarily through contact with an
               infected person when they cough or sneeze. It also spreads when a
               person touches a surface or object that has the virus on it, then
